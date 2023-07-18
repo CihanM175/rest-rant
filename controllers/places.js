65d382f5bd1d35bed8a5aaa14bfa02a66e743455
Cihan Mabocoglu <cihanmabocoglu@gmail.com>
2023-07-18 18:27:59 -0400

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -3,6 +3,11 @@ const Router = require('express').Router()
 Router.get('/new', (req, res) => {
   res.render('places/new')
 })
+ Router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+  })
+  
 
 Router.get('/', (req, res) => {
    let places = [{
@@ -22,25 +27,7 @@ Router.get('/', (req, res) => {
     res.render('places/index', {places})
   })
   
-  Router.post('/', (req, res) => {
-    let places = [{
-         name: 'H-Thai-ML',
-         city: 'Seattle',
-         state: 'WA',
-         cuisines: 'Thai, Pan-Asian',
-         pic: 'http://placekitten.com/250/250'
-       }, {
-         name: 'Coding Cat Cafe',
-         city: 'Phoenix',
-         state: 'AZ',
-         cuisines: 'Coffee, Bakery',
-         pic: 'http://placekitten.com/250/250'
-       }]
-       
-     res.render('places/index', {places})
-   })
-   
-
+ 
 
 module.exports = Router
 
