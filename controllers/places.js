fbe44f378dadde5c5e272eb8b3d246c7954bb7d7
Cihan Mabocoglu <cihanmabocoglu@gmail.com>
2023-07-18 18:03:35 -0400

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -22,6 +22,25 @@ Router.get('/', (req, res) => {
     res.render('places/index', {places})
   })
   
+  Router.post('/', (req, res) => {
+    let places = [{
+         name: 'H-Thai-ML',
+         city: 'Seattle',
+         state: 'WA',
+         cuisines: 'Thai, Pan-Asian',
+         pic: 'http://placekitten.com/250/250'
+       }, {
+         name: 'Coding Cat Cafe',
+         city: 'Phoenix',
+         state: 'AZ',
+         cuisines: 'Coffee, Bakery',
+         pic: 'http://placekitten.com/250/250'
+       }]
+       
+     res.render('places/index', {places})
+   })
+   
+
 
 module.exports = Router
 
